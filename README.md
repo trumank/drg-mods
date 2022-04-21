abe1930dd9a9dd864c0e0eecd4a5fd51ab1ff1ae
Truman Kilen <t@kilen.me>
2022-04-20 22:04:53 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# AssemblyStorms's DRG Mods
+
+Source for my blueprint mods is under `Content/_AssemblyStorm/`
+
+- https://drg.mod.io/sandbox-utilties
+- https://drg.mod.io/custom-difficulty
+- https://drg.mod.io/mission-selector
+
+Maintained versions of [ArcticEcho's mods](https://github.com/ArcticEcho/My-DRG-Mods)
+- https://drg.mod.io/better-spectator-reloaded
---
+++ Source/FSDEngine/FSDEngine.Build.cs
@@ -0,0 +1,17 @@
+using UnrealBuildTool;
+
+public class FSDEngine : ModuleRules {
+    public FSDEngine(ReadOnlyTargetRules Target) : base(Target) {
+        PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+        bLegacyPublicIncludePaths = false;
+        ShadowVariableWarningLevel = WarningLevel.Warning;
+        
+        PublicDependencyModuleNames.AddRange(new string[] {
+            "Chaos",
+            "Core",
+            "CoreUObject",
+            "Engine",
+            "PhysicsCore",
+        });
+    }
+}
