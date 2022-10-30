76971a6ca15cc9ee4c7a2a1bd2b24b4363a84abb
Truman Kilen <t@kilen.me>
2022-10-30 16:42:50 -0500

    SU update

--- Source/FSD/Public/DeepRepPath.h
+++ Source/FSD/Public/DeepRepPath.h
@@ -11,17 +11,18 @@ public:
     UPROPERTY(BlueprintReadWrite, EditAnywhere, Transient, meta=(AllowPrivateAccess=true))
     FVector PathBase;
     
-    UPROPERTY(EditAnywhere, Transient)
+    UPROPERTY(BlueprintReadWrite, EditAnywhere, Transient)
     uint8 PathLength;
     
     UPROPERTY(BlueprintReadWrite, EditAnywhere, Transient, meta=(AllowPrivateAccess=true))
     EDeepMovementState State;
     
-    UPROPERTY(EditAnywhere, Transient)
+    UPROPERTY(BlueprintReadWrite, EditAnywhere, Transient)
     uint8 StateBits;
     
-    UPROPERTY(EditAnywhere, Transient)
-    FVector PathOffsets[16];
+    UPROPERTY(BlueprintReadWrite, EditAnywhere, Transient)
+    TArray<FVector> PathOffsets;
+    // FVector PathOffsets[16];
     
     FSD_API FDeepRepPath();
 };
